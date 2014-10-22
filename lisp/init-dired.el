dac7dc3b6348c30c24834f57e51787b39c2d7c4a
sorpa'as plat <sorpaas@gmail.com>
2014-10-22 13:58:49 +0800

    toggle small dired hide details mode

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -15,4 +15,26 @@
             (lambda () (guide-key/add-local-guide-key-sequence "%"))))
 
 
+(defun set-window-width (n)
+  "Set the selected window's width."
+  (adjust-window-trailing-edge (selected-window) (- n (window-width)) t))
+
+(defun sorpaas/speed-dired-set-columns ()
+  "Set the selected window to 80 columns."
+  (set-window-width 22))
+
+(defun sorpaas/speed-dired-install ()
+  "Install speed dired"
+  (eval-after-load "dired"
+    '(progn
+       (define-key dired-mode-map (kbd "<f1>") 'sorpaas/speed-dired-toggle))))
+
+(defun sorpaas/speed-dired-toggle ()
+  "Speed up dired by auto change frame size and toggle dired details"
+  (interactive)
+  (dired-hide-details-mode)
+  (sorpaas/speed-dired-set-columns))
+
+(sorpaas/speed-dired-install)
+
 (provide 'init-dired)
