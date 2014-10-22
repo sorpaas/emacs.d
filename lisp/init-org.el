a2b328263f2e4c41608ae7d41844260a12371bc8
sorpa'as plat <sorpaas@gmail.com>
2014-10-22 14:29:35 +0800

    add org agenda and tag functionality

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -17,6 +17,10 @@
       org-agenda-span 14
       org-agenda-include-diary t
       org-agenda-window-setup 'current-window
+      org-agenda-tags-todo-honor-ignore-options t
+      org-agenda-skip-deadline-if-done t
+      org-agenda-skip-scheduled-if-done t
+      org-agenda-ndays 7
       org-fast-tag-selection-single-key 'expert
       org-html-validation-link nil
       org-export-kill-product-buffer-when-displayed t
@@ -32,10 +36,235 @@
 ; Targets complete in steps so we start with filename, TAB shows the next level of targets etc
 (setq org-outline-path-complete-in-steps t)
 
-
+;;; - Org: TODO Keywords
 (setq org-todo-keywords
-      (quote ((sequence "TODO(t)" "STARTED(s)" "|" "DONE(d!/!)")
-              (sequence "WAITING(w@/!)" "SOMEDAY(S)" "|" "CANCELLED(c@/!)"))))
+      (quote ((sequence "TODO(t)" "NEXT(n)" "WAITING(w@/!)" "|" "DONE(d)")
+              (sequence "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "SUB(s)")
+              (sequence "CHECKPOINT(c)" "|" "DONE(d)"))))
+
+(setq org-todo-keyword-faces
+      (quote (("TODO" :foreground "black" :background "red" :weight bold)
+              ("NEXT" :foreground "black" :background "blue" :weight bold)
+              ("DONE" :foreground "black" :background "forest green" :weight bold)
+              ("CHECKPOINT" :foreground "black" :background "yellow" :weight bold)
+              ("WAITING" :foreground "black" :background "orange" :weight bold)
+              ("HOLD" :foreground "black" :background "magenta" :weight bold)
+              ("CANCELLED" :foreground "black" :background "forest green" :weight bold)
+              ("SUB" :foreground "black" :background "forest green" :weight bold))))
+
+;;; - Org: Tag
+; Tags with fast selection keys
+(setq org-tag-alist (quote (("list" . ?l)
+                            ("event" . ?e)
+                            ("export" . ?')
+                            ("PROJECT" . ?p)
+                            ("THOUGHT" . ?t)
+                            ("HOLD" . ?h)
+                            ("CANCELED" . ?c)
+                            ("DONE" . ?d)
+                            ("preparation" . ?w)
+                            ("interest" . ?v)
+                            ("future" . ?z)
+                            ("meta" . ?s)
+                            ("research" . ?r)
+                            ("need_deadline")
+                            ("info" . ?i))))
+
+; Inheritance Settings
+(setq org-tags-exclude-from-inheritance '("PROJECT" "HOLD" "CANCELED" "DONE" "list"))
+
+; Allow setting single tags without the menu
+(setq org-fast-tag-selection-single-key (quote expert))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Org agenda
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Stuck Projects
+(setq org-stuck-projects
+      '("+PROJECT/-HOLD-CANCELED-DONE" ("TODO" "NEXT") ("meta") "\\<IGNORE\\>"))
+
+(defvar bh/hide-scheduled-and-waiting-next-tasks t)
+
+(defun zin/org-agenda-skip-tag (tag &optional others)
+  "Skip all entries that correspond to TAG.
+
+If OTHERS is true, skip all entries that do not correspond to TAG."
+  (let ((next-headline (save-excursion (or (outline-next-heading) (point-max))))
+        (current-headline (or (and (org-at-heading-p)
+                                   (point))
+                              (save-excursion (org-back-to-heading)))))
+    (if others
+        (if (not (member tag (org-get-tags-at current-headline)))
+            next-headline
+          nil)
+      (if (member tag (org-get-tags-at current-headline))
+          next-headline
+        nil))))
+
+(defun sp/org-agenda-skip-tag-and-scheduled (tag)
+  "Skip all entries that correspond to TAG or has been scheduled."
+  (let ((next-headline (save-excursion (or (outline-next-heading) (point-max))))
+        (current-headline (or (and (org-at-heading-p)
+                                   (point))
+                              (save-excursion (org-back-to-heading)))))
+    (if (equal next-headline (zin/org-agenda-skip-tag tag))
+        next-headline
+      (org-agenda-skip-entry-if 'scheduled))))
+
+(defun sp/org-agenda-skip-tag-and-not-deadline (tag)
+  "Skip all entried that correspond to TAG and has no deadline."
+   (let ((next-headline (save-excursion (or (outline-next-heading) (point-max))))
+        (current-headline (or (and (org-at-heading-p)
+                                   (point))
+                              (save-excursion (org-back-to-heading)))))
+     (if (and (member tag (org-get-tags-at current-headline)) (not (org-get-deadline-time current-headline)))
+         next-headline
+       nil)))
+
+;;; - Org: Agenda Commands
+(setq org-agenda-custom-commands
+      '(("n" "Notes" tags "NOTE"
+         ((org-agenda-overriding-header "Notes")
+          (org-tags-match-list-sublevels t)))
+        ("h" "Habits" tags-todo "STYLE=\"habit\""
+         ((org-agenda-overriding-header "Habits")
+          (org-agenda-sorting-strategy
+           '(todo-state-down effort-up category-keep))))
+        (" " "Agenda"
+         ((agenda "" nil)
+          (tags "REFILE"
+                ((org-agenda-overriding-header "Tasks to Refile")
+                 (org-tags-match-list-sublevels nil)))
+          (tags-todo "-CANCELLED/!"
+                     ((org-agenda-overriding-header "Stuck Projects")
+                      (org-agenda-skip-function 'bh/skip-non-stuck-projects)
+                      (org-agenda-sorting-strategy
+                       '(category-keep))))
+          (tags-todo "-HOLD-CANCELLED/!"
+                     ((org-agenda-overriding-header "Projects")
+                      (org-agenda-skip-function 'bh/skip-non-projects)
+                      (org-tags-match-list-sublevels 'indented)
+                      (org-agenda-sorting-strategy
+                       '(category-keep))))
+          (tags-todo "-CANCELLED/!NEXT"
+                     ((org-agenda-overriding-header (concat "Project Next Tasks"
+                                                            (if bh/hide-scheduled-and-waiting-next-tasks
+                                                                ""
+                                                              " (including WAITING and SCHEDULED tasks)")))
+                      (org-agenda-skip-function 'bh/skip-projects-and-habits-and-single-tasks)
+                      (org-tags-match-list-sublevels t)
+                      (org-agenda-todo-ignore-scheduled bh/hide-scheduled-and-waiting-next-tasks)
+                      (org-agenda-todo-ignore-deadlines bh/hide-scheduled-and-waiting-next-tasks)
+                      (org-agenda-todo-ignore-with-date bh/hide-scheduled-and-waiting-next-tasks)
+                      (org-agenda-sorting-strategy
+                       '(todo-state-down effort-up category-keep))))
+          (tags-todo "-REFILE-CANCELLED-WAITING-HOLD/!"
+                     ((org-agenda-overriding-header (concat "Project Subtasks"
+                                                            (if bh/hide-scheduled-and-waiting-next-tasks
+                                                                ""
+                                                              " (including WAITING and SCHEDULED tasks)")))
+                      (org-agenda-skip-function 'bh/skip-non-project-tasks)
+                      (org-agenda-todo-ignore-scheduled bh/hide-scheduled-and-waiting-next-tasks)
+                      (org-agenda-todo-ignore-deadlines bh/hide-scheduled-and-waiting-next-tasks)
+                      (org-agenda-todo-ignore-with-date bh/hide-scheduled-and-waiting-next-tasks)
+                      (org-agenda-sorting-strategy
+                       '(category-keep))))
+          (tags-todo "-REFILE-CANCELLED-WAITING-HOLD/!"
+                     ((org-agenda-overriding-header (concat "Standalone Tasks"
+                                                            (if bh/hide-scheduled-and-waiting-next-tasks
+                                                                ""
+                                                              " (including WAITING and SCHEDULED tasks)")))
+                      (org-agenda-skip-function 'bh/skip-project-tasks)
+                      (org-agenda-todo-ignore-scheduled bh/hide-scheduled-and-waiting-next-tasks)
+                      (org-agenda-todo-ignore-deadlines bh/hide-scheduled-and-waiting-next-tasks)
+                      (org-agenda-todo-ignore-with-date bh/hide-scheduled-and-waiting-next-tasks)
+                      (org-agenda-sorting-strategy
+                       '(category-keep))))
+          (tags-todo "-CANCELLED+WAITING|HOLD/!"
+                     ((org-agenda-overriding-header "Waiting and Postponed Tasks")
+                      (org-agenda-skip-function 'bh/skip-stuck-projects)
+                      (org-tags-match-list-sublevels nil)
+                      (org-agenda-todo-ignore-scheduled t)
+                      (org-agenda-todo-ignore-deadlines t)))
+          (tags "-REFILE/"
+                ((org-agenda-overriding-header "Tasks to Archive")
+                 (org-agenda-skip-function 'bh/skip-non-archivable-tasks)
+                 (org-tags-match-list-sublevels nil))))
+         nil)
+      ("p" "All projects"
+       ((tags "+PROJECT+meta"
+              ((org-agenda-overriding-header "Meta Planning Projects")
+               (org-agenda-prefix-format " %t %s")
+               (org-agenda-sorting-strategy '(alpha-up))))
+        (tags "+PROJECT+future"
+              ((org-agenda-overriding-header "Future Projects")
+               (org-agenda-prefix-format " %t %s")
+               (org-agenda-sorting-strategy '(alpha-up))))
+        (tags "+PROJECT+interest"
+              ((org-agenda-overriding-header "Interest Projects")
+               (org-agenda-prefix-format " %t %s")
+               (org-agenda-sorting-strategy '(alpha-up))))
+        (tags "+PROJECT+preparation"
+              ((org-agenda-overriding-header "Preparation Projects")
+               (org-agenda-prefix-format " %t %s")
+               (org-agenda-sorting-strategy '(alpha-up))))
+        (tags "+PROJECT+future+meta"
+              ((org-agenda-overriding-header "Future Meta Projects")
+               (org-agenda-prefix-format " %t %s")
+               (org-agenda-sorting-strategy '(alpha-up))))
+        (tags "+PROJECT+interest+meta"
+              ((org-agenda-overriding-header "Interest Meta Projects")
+               (org-agenda-prefix-format " %t %s")
+               (org-agenda-sorting-strategy '(alpha-up))))
+        (tags "+PROJECT+preparation+meta"
+              ((org-agenda-overriding-header "Preparation Meta Projects")
+               (org-agenda-prefix-format " %t %s")
+               (org-agenda-sorting-strategy '(alpha-up))))
+        (tags "+PROJECT+preparation+interest"
+              ((org-agenda-overriding-header "Both Preparation and Interest Projects")
+               (org-agenda-prefix-format " %t %s")
+               (org-agenda-sorting-strategy '(alpha-up))))
+        (stuck ""
+               ((org-agenda-overriding-header "Stuck Projects")
+                (org-agenda-prefix-format " %t %s")
+                (org-agenda-sorting-strategy '(alpha-up))))
+        (tags "+PROJECT/-HOLD-CANCELED-DONE"
+              ((org-agenda-overriding-header "Active Projects")
+               (org-agenda-prefix-format " %t %s")
+               (org-agenda-sorting-strategy '(alpha-up))))
+        (org-agenda-files)))
+      ("i" "Ideas"
+       ((tags "idea")
+        ))
+      ("d" "Today's plan"
+       ((agenda "" ((org-agenda-ndays 2)
+                    ))
+        (todo "NEXT" ((org-agenda-overriding-header "Next Planned Tasks")
+                      ))
+        (todo "TODO" ((org-agenda-overriding-header "Unscheduled Tasks")
+                      (org-agenda-todo-ignore-scheduled t)))
+        (agenda "" ((org-agenda-overriding-header "Upcoming Deadlines")
+                    (org-agenda-ndays 1)
+                    (org-agenda-entry-types '(:deadline))
+                    (org-deadline-warning-days 7)
+                    (org-agenda-time-grid nil)))
+        (todo "WAITING" ((org-agenda-overriding-header "Waiting Tasks")
+                         ))
+        (tags "REFILE"
+              ((org-agenda-overriding-header "Tasks to Refile")
+               (org-tags-match-list-sublevels nil)))
+        (stuck ""
+               ((org-agenda-overriding-header "Stuck Projects")
+                (org-agenda-prefix-format " %t %s")
+                (org-agenda-sorting-strategy '(alpha-up))))
+        (tags "+PROJECT+meta"
+              ((org-agenda-overriding-header "Meta Projects")
+               (org-agenda-prefix-format " %t %s")
+               (org-agenda-sorting-strategy '(alpha-up))))
+        (tags "+PROJECT-meta/-HOLD-CANCELED-DONE"
+              ((org-agenda-overriding-header "All Other Projects")
+               (org-agenda-prefix-format " %t %s")
+               (org-agenda-sorting-strategy '(alpha-up))))))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
